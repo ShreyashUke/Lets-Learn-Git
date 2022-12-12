1ae73b95a7ec4877b6c6aa7a883fec0efc39052e
ASHU <workwithashu001@gmail.com>
2022-12-13 00:36:54 +0530

    Changes made

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Multiply {
     public static void main(String args[]) {
         int a=5;
         int b=6;
-        System.out.println("The product is"+(a*b));
+        System.out.println("The product of a and b  is"+(a*b));
         
     }
     
